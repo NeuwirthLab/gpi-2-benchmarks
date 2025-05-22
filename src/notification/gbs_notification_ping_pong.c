6ac53363af8a86cb0db9abd78b6992e2d2ba702e
jwarczok <jwarczok@students.uni-mainz.de>
2025-05-22 14:58:30 +0200

    Code aufgeräumt, jetzt scheinbar schneller

--- src/notification/gbs_notification_ping_pong.c
+++ src/notification/gbs_notification_ping_pong.c
@@ -18,17 +18,25 @@ main(int argc, char* argv[])
 
   bo_ret = benchmark_options(argc, argv);
 
+  init_comm_library(&my_id, &num_pes);
+
   switch(bo_ret)
   {
   case OPTIONS_BAD_USAGE:
-    print_bad_usage();
+    if (my_id == 0)
+    {
+      print_bad_usage();
+      print_help_message();
+    }
     return EXIT_FAILURE;
   case OPTIONS_HELP:
-    print_help_message();
+    if (my_id == 0)
+    {
+      print_help_message();
+    }
     return EXIT_SUCCESS;
   }
 
-  init_comm_library(&my_id, &num_pes);
   if(num_pes > 2)
   {
     fprintf(stderr, "Benchmark requires exactly two processes!\n");
@@ -55,8 +63,8 @@ main(int argc, char* argv[])
         time = Wtime();
       }
       GASPI_CHECK(
-          gaspi_notify(segment_id, 1, i, notification_val, q_id, GASPI_BLOCK));
-      GASPI_CHECK(gaspi_notify_waitsome(segment_id, i, 1, &notification_id,
+          gaspi_notify(segment_id, 1, notification_id, notification_val, q_id, GASPI_BLOCK));
+      GASPI_CHECK(gaspi_notify_waitsome(segment_id, notification_id, 1, &notification_id,
                                         GASPI_BLOCK));
       if(i >= options.skip)
       {
@@ -65,9 +73,9 @@ main(int argc, char* argv[])
     }
     else
     {
-      GASPI_CHECK(gaspi_notify_waitsome(segment_id, i, 1, &notification_id,
+      GASPI_CHECK(gaspi_notify_waitsome(segment_id, notification_id, 1, &notification_id,
                                         GASPI_BLOCK));
-      GASPI_CHECK(gaspi_notify(segment_id, 0, i, my_id, q_id, GASPI_BLOCK));
+      GASPI_CHECK(gaspi_notify(segment_id, 0, notification_id, notification_val, q_id, GASPI_BLOCK));
     }
     GASPI_CHECK(gaspi_wait(q_id, GASPI_BLOCK));
   }
